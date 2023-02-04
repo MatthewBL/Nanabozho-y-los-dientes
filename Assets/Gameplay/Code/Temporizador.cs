8c004818c276fe202fc8d6bcac0f24a461410576
Matthew Bwye Lera <73072943+MatthewBL@users.noreply.github.com>
2023-02-04 14:42:58 +0100

    Temporizador

--- Assets/Gameplay/Code/Temporizador.cs
+++ Assets/Gameplay/Code/Temporizador.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Temporizador : MonoBehaviour
 {
     public float tiempoInicial = 143;
     public float tiempoRestante = 143;
+
+    public TextMeshProUGUI text;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,10 @@ public class Temporizador : MonoBehaviour
     void Update()
     {
         tiempoRestante -= Time.deltaTime;
+        int minutos = Mathf.FloorToInt(tiempoRestante/60);
+        int segundos = Mathf.FloorToInt(tiempoRestante % 60);
+        if (segundos >= 10) text.text = minutos + ":" + segundos;
+        else text.text = minutos + ":" + "0" + segundos;
     }
 
     public float RatioTiempo()
