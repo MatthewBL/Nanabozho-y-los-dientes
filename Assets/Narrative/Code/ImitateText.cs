517e65f9668081cebcfac0129eb5bdd8640c15ca
Matthew Bwye Lera <73072943+MatthewBL@users.noreply.github.com>
2023-02-05 14:05:26 +0100

    Añadida sombra a los subtítulos

--- Assets/Narrative/Code/ImitateText.cs
+++ Assets/Narrative/Code/ImitateText.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ImitateText : MonoBehaviour
 {
     public Text text;
+    public TextMeshProUGUI textPro;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Text>().text = text.text;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (text)
+        {
+            GetComponent<Text>().text = text.text;
+        }
+        if (textPro)
+        {
+            GetComponent<TextMeshProUGUI>().text = textPro.text;
+        }
     }
 }
